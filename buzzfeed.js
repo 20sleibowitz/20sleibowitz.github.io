f14712ee9e1339d65165d735388013fff844760e
20sleibowitz <44878090+20sleibowitz@users.noreply.github.com>
2019-02-12 19:21:14 -0500

    Update buzzfeed.js

--- buzzfeed.js
+++ buzzfeed.js
@@ -1,6 +1,6 @@
 function whenButtonClicked(){
  element b =document.getElementById("banner").getElementById("one");
- b.innerHTML = "YOU CLICKED ME!";
+ b.innerHTML = "color??";
  b.style.size = 400%;
  b.style.color = "Lime";
 }
