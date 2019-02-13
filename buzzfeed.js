e2e0c96c7b3f3ee0d6bc79e51571c282cfad63cf
20sleibowitz <44878090+20sleibowitz@users.noreply.github.com>
2019-02-12 19:19:03 -0500

    Update buzzfeed.js

--- buzzfeed.js
+++ buzzfeed.js
@@ -1,5 +1,5 @@
 function whenButtonClicked(){
- element b =document.getElementById("banner")
+ element b =document.getElementById("banner");
  b.innerHTML = "YOU CLICKED ME!";
  b.style.size = 400%;
  b.style.color = "Lime";
