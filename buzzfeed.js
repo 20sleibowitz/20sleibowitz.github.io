e88e96d942cbe303ba6d6b6b738dd151bbffe17c
20sleibowitz <44878090+20sleibowitz@users.noreply.github.com>
2019-02-12 19:08:13 -0500

    Update buzzfeed.js

--- buzzfeed.js
+++ buzzfeed.js
@@ -1,3 +1,3 @@
-$('enterbutt').on('click', function(e) {
-   document.body.innerHTML = "it worked i guess";
+function whenButtonClicked(){
+ document.getElementById("enterbutt").innerHTML = "YOU CLICKED ME!";  
 }
